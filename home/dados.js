75e8a9d8d0391f861d3ee998a79d3d1871eaaa79
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2023-12-11 13:05:06 -0300

    Create dados.js

---
+++ home/dados.js
@@ -0,0 +1,9 @@
+var quantidade =10;
+var preco = quantidade * quantidade;
+
+console.log("caneta", quantidade);
+console.log(preco);
+console.log(1.4);
+console.log(6.4);
+console.log(7.4);
+console.log(0.4);
