29cb94204db5c15e7a6e5eb5c2c6f078ebd75186
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2023-12-13 15:29:25 -0300

    Create usandoVar1.js

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Seraá???' 
+            } 
+        } 
+    } 
+}
+
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local)// var criada dentro da funciton é só é possivel ser acessada dentro da function.
