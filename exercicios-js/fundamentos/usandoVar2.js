d9269feab7336853fc79a37a3a9f4f798cd35019
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2023-12-14 13:40:22 -0300

    14122023

---
+++ exercicios-js/fundamentos/usandoVar2.js
@@ -0,0 +1,7 @@
+//var conta como uma variavel global então o numero 2 vai ser inserido em tudo que tiver a variavle numero.
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro = ', numero) 
+}
+console.log('fora = ',numero )
