4beb69cd412958d590715392fcb7059dcb4b7be0
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2023-12-19 12:39:52 -0300

    19122023

--- exercicios-js/function/functionsAnomimas.js
+++ exercicios-js/function/functionsAnomimas.js
@@ -11,10 +11,10 @@ imprimirResultado(3, 4, soma)
 imprimirResultado(3, 4, function (x, y) {
     return x - y
 })
-imprimirResultado(3,4,(x,y) => x* y)
+imprimirResultado(3, 4, (x, y) => x * y)
 
 const pessoa = {
-    falar: function(){
+    falar: function () {
         console.log('Opa')
     }
 }
