1e728e5c9091621862645d7014e73de1327dd59b
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2023-12-21 23:06:05 -0300

    21122023

---
+++ exercicios-js/Exercicios/exercicio(36).js
@@ -0,0 +1,29 @@
+// 36) Crie duas funções que recebem dois parâmetros, um vetor com apenas valores numéricos e um número
+// inteiro. Faça com que a primeira função retorne outro vetor que será resultado da multiplicação de cada
+// elemento pelo número passado como parâmetro. A segunda função fará o mesmo da primeira se e somente se
+// o valor do elemento for maior que 5.
+let meuVetor = [2, 6, 10];
+let numero = 3;
+
+function multiplicaVetor(vetor, numero) {
+    let resultado = [];
+    for (let valor of vetor) {
+        resultado.push(valor * numero);
+    }
+    return resultado;
+}
+
+function multiplicaSeMaiorQueCinco(vetor, numero) {
+    let resultado = [];
+    for (let valor of vetor) {
+        if (valor > 5) {
+            resultado.push(valor * numero);
+        } else {
+            resultado.push(valor);
+        }
+    }
+    return resultado;
+}
+
+console.log(multiplicaVetor(meuVetor, numero)); // Deve retornar [6, 18, 30]
+console.log(multiplicaSeMaiorQueCinco(meuVetor, numero)); // Deve retornar [2, 18, 30]
